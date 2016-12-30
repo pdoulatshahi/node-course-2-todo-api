cfffdd11f6de101a76bb35a71b476fe7b69f1e5f
pdoulatshahi <paul.doulatshahi@gmail.com>
2016-12-29 22:42:39 -0800

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -118,13 +118,20 @@ app.post('/users/login', (req, res) => {
       res.header('x-auth', token).send(user);
     });
   }).catch((e) => {
-
-  })
-
+    res.status(400).send(e);
+  });
 })
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
